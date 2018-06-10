fdfca3ff385d88f3945fc2ad4d78bb65821e1a8e
hoto17296 <mail@hoto.me>
2018-06-10 18:45:26 +0900

    implement screensaver

---
+++ README.md
@@ -0,0 +1,15 @@
+# Time Leap Clock
+A macOS screen saver that displays a clock like the movie "時をかける少女 (The Girl Who Leapt Through Time)".
+
+![](image.png)
+
+## Installation
+0. Download the latest release from the [releases page](https://github.com/hoto17296/TimeLeapClock/releases) and unzip it.
+0. Double click the `timeleap100.ttf` file to install font to your computer.
+0. Double click the `TimeLeapClock.saver` file to install screen saver.
+
+## LICENSE
+- Font file `timeleap100.ttf` is licensed under a [Creative Commons Attribution 2.1 Japan](https://creativecommons.org/licenses/by/2.1/jp/) License.
+  - [kironono (Junichi Kakisako)](http://kironono.com)
+  - [時をかける少女 デザインフォント「TimeLeap」 - Ola Kae Tode Tai](http://ola.kironono.com/entry/fonts-timeleap)
+- Other files are licensed under [CC0](https://creativecommons.org/publicdomain/zero/1.0/deed.ja).
--- TimeLeapClock/TimeLeapClockView.swift
+++ TimeLeapClock/TimeLeapClockView.swift
@@ -1,4 +1,44 @@
 import ScreenSaver
 
 class TimeLeapClockView: ScreenSaverView {
+    
+    let dateFormatter = DateFormatter()
+    var textRect: NSRect!
+    var textAttributes: [String: Any] = [:]
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.setAttributes()
+    }
+    
+    override init?(frame: NSRect, isPreview: Bool) {
+        super.init(frame: frame, isPreview: isPreview)
+        self.setAttributes()
+    }
+    
+    func setAttributes() {
+        let w = self.bounds.size.width
+        let h = self.bounds.size.height
+        
+        self.dateFormatter.dateFormat = "yy:MM:dd:HH:mm:ss:SS"
+        
+        let fontSize = w / CGFloat(self.dateFormatter.dateFormat.count - 1)
+        self.textRect = NSMakeRect(0, 0, w, h / 2 + fontSize)
+        
+        let shadow = NSShadow()
+        shadow.shadowColor = NSColor.red
+        shadow.shadowBlurRadius = fontSize / 4.0
+        self.textAttributes = [
+            NSFontAttributeName: NSFont(name: "TimeLeap", size: fontSize),
+            NSForegroundColorAttributeName: NSColor.red,
+            NSShadowAttributeName: shadow,
+        ]
+    }
+    
+    override func animateOneFrame() {
+        NSBezierPath(rect: self.bounds).fill()
+        let text = self.dateFormatter.string(from: Date())
+        text.draw(in: self.textRect, withAttributes: self.textAttributes)
+    }
+    
 }
